88157dcf80a6ddc4ae13cb831f85f0c157661218
Bananasplit04 <mvrobledo@uninorte.edu.co>
2022-11-29 21:16:53 -0500

    Cambios de ruta

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -70,14 +70,14 @@ function Index(){
                 </div>
 
                 //Bootstrap core JavaScript
-                <script src="vendor/jquery/jquery.min.js"></script>
-                <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                <script src="../public/assets/js/vendor/jquery/jquery.min.js"></script>
+                <script src="../public/assets/js/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
                 // Core plugin JavaScript
-                <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+                <script src="../public/assets/js/vendor/jquery-easing/jquery.easing.min.js"></script>
 
                 //Custom scripts for all pages
-                <script src="js/sb-admin-2.min.js"></script>
+                <script src="../public/assets/js/sb-admin-2.min.js"></script>
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
             </Fragment>
             
